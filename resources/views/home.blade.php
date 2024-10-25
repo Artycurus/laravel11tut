58816408ad9f4ac3e3cccd7d1af341e1f37ac5c2
Artycurus <git>
2024-10-25 14:21:13 +0200

    les 6 klaar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,5 +3,4 @@
        Home Page
     </x-slot:heading>   
 
- <h1>{{ $greeting }}. from the Home page. My name is {{ $name }}.</h1>
-</x-layout>
+ </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,60 @@
 <?php
 
+use Illuminate\Support\arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home', [
-        'greeting' => 'yo',
-        'name' => 'Larry robot'
+    return view('home');
+
+});
+
+Route::get('/jobs', function () {
+    return view('jobs', [
+        'jobs' => [
+            [
+                'id' => 1,
+                'title' => 'Director',
+                'salary' => '$50,000'
+            ],
+            [
+                'id => 2',
+                'title' => 'Programmer',
+                'salary' => '$10,000'
+            ],
+            [
+                'id' => 3,             
+                'title' => 'Teacher',
+                'salary' => '$40,000'
+            ]
+        ]
     ]);
 });
 
-Route::get('/about', function () {
-    return view('about');
+Route::get('/jobs/{id}', function ($id) {
+    $jobs = [
+        [
+            'id' => 1,
+            'title' => 'Director',
+            'salary' => '$50,000'
+        ],
+        [
+            'id => 2',
+            'title' => 'Programmer',
+            'salary' => '$10,000'
+        ],
+        [
+            'id' => 3,             
+            'title' => 'Teacher',
+            'salary' => '$40,000'
+        ]
+        ];
+
+    $job = arr::first($jobs, fn($job) => $job['id'] = $id);
+    
+  
+    return view('job', ['job'=> $job]);
 });
+
 
 Route::get('/contact', function () {
     return view('contact');
